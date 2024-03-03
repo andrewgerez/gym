2c1f5ff31da6aaae735eef6bf345f3a48c9aa83f
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-02 23:10:17 -0300

    chore: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => {
-  console.log('HTTP server running! :)')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server running! :)')
+  })
