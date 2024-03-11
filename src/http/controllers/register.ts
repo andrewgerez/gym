2475440e15b79b7e177b5e8eed7e1180ff63ba4a
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-10 23:11:49 -0300

    feat: global error handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -23,7 +23,7 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
       reply.status(409).send({ message: err.message })
     }
 
-    reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
