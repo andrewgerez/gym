e928de2e4bec5d72a26e6e1883d18391742af4af
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-12 00:51:23 -0300

    feat: auth controller

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,29 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-user-repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(req.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+
+    await authenticateUseCase.execute({ email, password })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  return reply.status(200).send()
+}
