8bac93fc44c48fd2eedffd8d72a0be09c3d0b5c6
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 03:54:09 -0300

    feat: profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,19 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
   await req.jwtVerify()
 
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    },
+  })
 }
