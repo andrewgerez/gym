2269f8687236a2f9b6b60671a6002d41891334ac
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 03:47:09 -0300

    feat: create jwt

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,5 +1,6 @@
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { sign } from 'crypto'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
@@ -14,7 +15,21 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password
+    })
+
+    const token = await reply.jwtSign(
+      {}, {
+      sign: {
+        sub: user.id,
+      }
+    })
+
+    return reply.status(200).send({
+      token
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       reply.status(400).send({ message: err.message })
@@ -22,6 +37,4 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
 
     throw err
   }
-
-  return reply.status(200).send()
 }
