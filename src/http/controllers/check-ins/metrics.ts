b15f6d498e6e95c92487a55ee160d494dba5446b
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 17:53:00 -0300

    feat: get user metrics controller

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,14 @@
+import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function metrics(req: FastifyRequest, reply: FastifyReply) {
+  const getUserMetricsUseCase = makeGetUserMetricsUseCase()
+
+  const { checkInsCount } = await getUserMetricsUseCase.execute({
+    userId: req.user.sub,
+  })
+
+  return reply.status(200).send({
+    checkInsCount,
+  })
+}
