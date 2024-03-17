9ff6e1ff4bb3e0700395855884c3803269fdef19
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 03:51:12 -0300

    feat: jwt verify

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
+  await req.jwtVerify()
 
   return reply.status(200).send()
 }
