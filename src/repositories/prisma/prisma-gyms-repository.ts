45f9159330474a9009cbdf00b328cec42f60a824
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 00:59:15 -0300

    feat: prismagymsrepository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,43 @@
+import { Gym, Prisma } from "@prisma/client";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const user = await prisma.gym.findUnique({
+      where: {
+        id,
+      }
+    })
+
+    return user
+  }
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        }
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}
