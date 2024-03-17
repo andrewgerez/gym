8e7b2415d427d8da9c21c2bd067f51000bc5b52f
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 19:33:24 -0300

    feat: create refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,6 +1,5 @@
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
-import { sign } from 'crypto'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
@@ -27,9 +26,25 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
       }
     })
 
-    return reply.status(200).send({
-      token
+    const refreshToken = await reply.jwtSign(
+      {}, {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d',
+      }
     })
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token
+      })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       reply.status(400).send({ message: err.message })
