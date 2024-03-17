ff902822c30310ce76cc0b857cf18a7bb1ad1e91
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 19:38:03 -0300

    feat: refresh token controller

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,32 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function refresh(req: FastifyRequest, reply: FastifyReply) {
+  await req.jwtVerify({ onlyCookie: true })
+
+  const token = await reply.jwtSign(
+    {}, {
+    sign: {
+      sub: req.user.sub,
+    }
+  })
+
+  const refreshToken = await reply.jwtSign(
+    {}, {
+    sign: {
+      sub: req.user.sub,
+      expiresIn: '7d',
+    }
+  })
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token
+    })
+}
