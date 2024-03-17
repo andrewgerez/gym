310ef1eee693ca2f7a7a99316b92937848f4a830
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-17 17:51:55 -0300

    feat: fetch user check ins controller

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function history(req: FastifyRequest, reply: FastifyReply) {
+  const checkInHistoryQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page } = checkInHistoryQuerySchema.parse(req.query)
+
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+
+  const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
+    userId: req.user.sub,
+    page,
+  })
+
+  return reply.status(200).send({
+    checkIns,
+  })
+}
