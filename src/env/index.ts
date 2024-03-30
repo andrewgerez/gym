3c6a3106cb2201cac831608d454a2fc18e1ff9d8
Andrew Gerez <88686031+andrewgerez@users.noreply.github.com>
2024-03-30 18:44:47 -0300

    refactory: environment

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,9 +11,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('Invalid environemnt variables:', _env.error.format())
+  console.error('Invalid environment variables:', _env.error.format())
 
-  throw new Error('Invalid environemnt variables.')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
